ce0c10b8659be5c14ff9462070a88bdaa95b3a66
RealShiftless <aaronmoulding@live.nl>
2025-10-18 20:14:52 +0200

    Fixed ram.c refrences

--- src/cartridge/ram.c
+++ src/cartridge/ram.c
@@ -18,20 +18,20 @@ static inline uint32_t map_address(uint8_t banks, uint16_t address) {
 
 // Device func
 static uint8_t on_read(Km8Context* ctx, uint16_t address) {
-    if(ctx->cartridge->ram_banks == 0) {
+    if(ctx->cartridge->ram_bank_count == 0) {
         return 0;
     }
 
-    uint32_t mapped_address = map_address(ctx->cartridge->ram_banks, address);
+    uint32_t mapped_address = map_address(ctx->cartridge->ram_bank_count, address);
 
     return ctx->cartridge->ram[mapped_address];
 }
 static void on_write(Km8Context* ctx, uint16_t address, uint8_t value) {
-    if(ctx->cartridge->ram_banks == 0) {
+    if(ctx->cartridge->ram_bank_count == 0) {
         return;
     }
 
-    uint32_t mapped_address = map_address(ctx->cartridge->ram_banks, address);
+    uint32_t mapped_address = map_address(ctx->cartridge->ram_bank_count, address);
 
     ctx->cartridge->ram[mapped_address] = value;
 }
