09cf1f0f314a4b213030b62bbbd27ca9f696512e
RealShiftless <aaronmoulding@live.nl>
2025-10-18 20:13:30 +0200

    Fixed naming on GLFW platform.

--- platforms/glfw/src/main.c
+++ platforms/glfw/src/main.c
@@ -7,21 +7,20 @@ int main(int argc, char** argv) {
     (void)argc;
     (void)argv;
 
-    WindowErr init_result = km8_window_init();
-    if (init_result != KM8_WINDOW_OK) {
-        fprintf(stderr, "km8: window init failed: %s\n", km8_window_get_err_string(init_result));
+    if (platform_glfw_init()) {
+        fprintf(stderr, "Failed to initialize glfw!");
         return 1;
     }
 
-    while (!km8_window_should_close()) {
-        km8_window_poll_events();
+    while (!platform_glfw_should_close()) {
+        platform_glfw_poll_events();
 
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        km8_window_swap_buffers();
+        platform_glfw_swap_buffers();
     }
 
-    km8_window_shutdown();
+    platform_glfw_shutdown();
     return 0;
 }
